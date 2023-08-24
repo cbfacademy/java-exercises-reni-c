8a110ad04c797d2f8a75c69cc1620da7cc00366f
Reni Ogun-Coker <reni.coker@hotmail.co.uk>
2023-08-24 19:08:41 +0100

    first attempt at this exercise

--- exercises/input-output/src/main/java/com/cbfacademy/Read.java
+++ exercises/input-output/src/main/java/com/cbfacademy/Read.java
@@ -1,44 +1,40 @@
 package com.cbfacademy;
 
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.io.OutputStreamWriter;
+  import java.io.BufferedWriter;
+  import java.io.FileInputStream;
+  import java.io.FileOutputStream;
+  import java.io.IOException;
+  import java.io.InputStreamReader;
+  import java.io.LineNumberReader;
+  import java.io.OutputStreamWriter; 
 
 public class Read {
     public static void main(String[] args) {
-        String filePath = "input-output/src/main/resources/exercise.txt";
-
+        String inputFilePath = "input-output/src/main/resources/exercise.txt";
+        String outputFilePath = "input-output/src/main/resources/exerciseNew.txt";
+ 
         try(
-            FileInputStream file = new FileInputStream(filePath);
-            InputStreamReader streamReader = new InputStreamReader(file);
-            LineNumberReader lineReader = new LineNumberReader(streamReader);
-        ) {
-            String outputLine = lineReader.readLine();
-            while (outputLine != null) {
-                System.out.printf("%d: %s%n", lineReader.getLineNumber(), outputLine);
-                outputLine = lineReader.readLine();
-            }      
-        } catch (IOException e) {
-            System.out.println("File reading exception: " + e.getMessage());
-        }
+            FileInputStream inputFile = new FileInputStream(inputFilePath);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputFile);
+            LineNumberReader lineReader = new LineNumberReader(inputStreamReader);
 
-        // String fileName = "exerciseNew.txt";
-        String fileName = "input-output/src/main/resources/exerciseNew.txt";
-
-        try(
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            FileOutputStream outputFile = new FileOutputStream(outputFilePath);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputFile);
             BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
         ) {
-            bufferedWriter.write(fileName);
+            String outputLine = lineReader.readLine();
 
-            bufferedWriter.close();
-            outputStreamWriter.close();
-            fileOutputStream.close();
+            while (outputLine != null) {
+                System.out.printf("%d: %s%n", lineReader.getLineNumber(), outputLine);
+                bufferedWriter.write(outputLine);
+                bufferedWriter.newLine();
+                outputLine = lineReader.readLine();
+
+                //bufferedWriter.close();
+                //outputStreamWriter.close();
+                //fileOutputStream.close(); 
+        }      
+
         } catch (IOException e) {
             System.out.println("File writing exception: " + e.getMessage());
         }
